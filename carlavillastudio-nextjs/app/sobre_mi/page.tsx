f291c399b538eac24e07afbbeb6527c089e0ee8f
Pablo Ferreras <pabloferreras@MyMacbookPro-8.local>
2025-03-27 09:36:11 +0100

    about

--- carlavillastudio-nextjs/app/sobre_mi/page.tsx
+++ carlavillastudio-nextjs/app/sobre_mi/page.tsx
@@ -6,16 +6,18 @@ export default function Page() {
     <div className="SobreMi">
       <div className="sobre_mi_container p-[50px] md:max-w-[1600px] mx-auto">
         <div className="flex flex-col md:flex-row items-center">
-          <Image
-          className="p-[50px]"
-          src="/images/sobre_mi/sobre_mi_carlavillastudio01.jpg"
-          alt="Imagen de perfil"  
-          width={700}
-          height={700}
-          />
-          <div>
-            <h1 className="text-5xl font-bold">María Carlavilla</h1>
-            <h2 className="text-3xl mt-[20px] font-light italic">Improvisando cuidadosamente</h2>
+          <div className="md:w-f2">
+            <Image
+            className="md:p-[50px]"
+            src="/images/sobre_mi/sobre_mi_carlavillastudio01.jpg"
+            alt="Imagen de perfil"  
+            width={700}
+            height={700}
+            />
+          </div>
+          <div className="mt-[50px] md:mt-[0px] md:w-f2">
+            <h1 className="text-7xl font-bold">María Carlavilla</h1>
+            <h2 className="text-5xl mt-[20px] font-light italic">Improvisando cuidadosamente</h2>
             <p className="text-xl font-extralight mt-[10px]">
               Tras graduarme en ADE y dedicarme al mundo de la moda, descubrí que mi verdadera pasión residía 
               en comprender el producto, de modo que decidí dar un giro a mi carrera y estudiar patronaje y moda. 
@@ -31,16 +33,16 @@ export default function Page() {
             </p>
           </div>
         </div>
-        <div>
-          <div className="w-[100%] flex">
-            <div className="flex md:w-[820px] ml-auto">
-              <SvgQuotation className="w-[100px] fill-gray-300 translate-y-[-60px]" />
-              <span className="text-5xl font-light italic w-[720px]">
+        <div className="mt-[50px]">
+          <div className="w-[100%] flex ">
+            <div className="flex md:w-f2 items-start ml-auto">
+              <SvgQuotation className="w-[15%] md:w-[100px] fill-gray-300 md:translate-y-[-15px]" />
+              <span className="text-3xl md:text-5xl font-light italic w-[85%] md:w-[720px]">
               Me encanta el patrón, ver la técnica que hay detrás de una prenda y cómo está hecha en plano
               </span>
             </div>
           </div>
-          <div className="flex items-end">
+          <div className="flex flex-col md:flex-row items-end">
             <div className="m-[20px]">
               <Image
               src="/images/sobre_mi/sobre_mi_carlavillastudio02.jpg"
@@ -57,6 +59,38 @@ export default function Page() {
               height={700}
               />
             </div>
+          </div>
+        </div>
+        <div className="flex flex-col md:flex-row items-center mt-[50px]">
+          <div className="flex items-start w-[100%] md:w-f2">
+            <SvgQuotation className="w-[15%] md:w-[100px] fill-gray-300 translate-y-[-15px]" />
+            <span className="text-3xl md:text-5xl font-light italic w-[85%] md:w-[720px]">
+            Durante el proceso de tu pieza, elegimos juntas las telas. Buscamos en diferentes rincones y lugares, con historia y de calidad, para que sean ÚNICAS y especiales
+            </span>
+          </div>
+          <div className="mt-[50px] md:mt-[0px] md:m-[80px]">
+            <Image
+            src="/images/sobre_mi/sobre_mi_carlavillastudio04.jpg"
+            alt="Imagen de perfil"
+            width={700}
+            height={700}
+            />
+          </div>
+        </div>
+        <div className="flex flex-col md:flex-row-reverse items-center mt-[50px]">
+          <div className="flex items-start w-[100%] md:w-f2">
+            <SvgQuotation className="w-[15%] md:w-[100px] fill-gray-300 translate-y-[-15px]" />
+            <span className="text-3xl md:text-5xl font-light italic w-[85%] md:w-[720px]">
+            Dar un valor añadido al oficio de la confección, detrás de cada prenda hay una persona, el proceso conlleva mucho tiempo y hay una profesión que requiere un aprendizaje.             
+            </span>
+          </div>
+          <div className="mt-[50px] md:mt-[0px] md:m-[80px]">
+            <Image
+            src="/images/sobre_mi/sobre_mi_carlavillastudio05.jpg"
+            alt="Imagen de perfil"
+            width={700}
+            height={700}
+            />
           </div>
         </div>
       </div>
